d974666c86ab13dd19324ddd4645221a7b31dd78
TrainingScav <dnslwk123@naver.com>
2025-06-27 15:47:38 +0900

    first commit

---
+++ src/test/java/com/jobgoalin/workinfo/WorkinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobgoalin.workinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
