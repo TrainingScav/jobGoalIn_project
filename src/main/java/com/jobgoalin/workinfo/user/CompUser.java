e4326cd21f2bf4d0f761c724e8f91d1bdfe048cd
jibaek <tngus000777@gmail.com>
2025-06-30 12:02:35 +0900

    User_info db 1차 생성 완료

---
+++ src/main/java/com/jobgoalin/workinfo/user/CompUser.java
@@ -0,0 +1,47 @@
+package com.jobgoalin.workinfo.user;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+@Table(name = "user_tb")
+@Entity
+public class CompUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 사용자 이름 중복 방지를 위한 유니크 제약 조건 설정
+    @Column(unique = true)
+    private String comUserLoginId ;
+
+    private String comUserPassword;
+    private String compUserPhone;
+    private String compUserEmail;
+    private String compUserNickname;
+    private String compTypeCode;
+    private int compRegNumber;
+    private String compName;
+    private String
+
+    // 엔티티가 영속화 될 때 자동으로 pc 현재 시간을 설정
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    // 객체 생성 시 가독성과 안정성 향상
+    @Builder
+    public CompUser(Long id, String username, String password, String email, Timestamp createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/com/jobgoalin/workinfo/user/User.java
@@ -0,0 +1,49 @@
+package com.jobgoalin.workinfo.user;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+@Table(name = "user_info")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+
+    // 사용자 이름 중복 방지를 위한 유니크 제약 조건 설정
+    @Column(name = "user_name")
+	private String username;
+	private String userLoginId;
+	private String userPassWord;
+	@Column(unique = true)
+	private String userEmail;
+	private String userAddress;
+	private String userPhone;
+	private String userBirth;
+	private String userGender;
+	private String userNickName;
+	private String userCivilSerial;
+	@CreationTimestamp
+    private Timestamp userCreatedAt;
+	private int accessLevel;
+	private boolean userLockYn;
+	private int loginAttemptCont;
+
+     // 객체 생성 시 가독성과 안정성 향상
+    @Builder
+    public User(int id, String username, String password, String email, Timestamp createdAt) {
+        this.user_id = id;
+        this.username = username;
+        this.userPassWord = password;
+        this.userEmail = email;
+        this.userCreatedAt = createdAt;
+    }
+}
