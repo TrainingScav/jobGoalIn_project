21e472c6b3149e3554db5cfaa17da53f9231158c
jibaek <tngus000777@gmail.com>
2025-06-30 15:52:01 +0900

    관리자 회원 정보 및 기업 회원 정보 DB

--- src/main/java/com/jobgoalin/workinfo/user/User.java
+++ src/main/java/com/jobgoalin/workinfo/user/User.java
@@ -16,34 +16,44 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int user_id;
+    private int userId;
 
-    // 사용자 이름 중복 방지를 위한 유니크 제약 조건 설정
-    @Column(name = "user_name")
+	@Column(nullable = false)
 	private String username;
+	@Column(unique = true ,nullable = false)
 	private String userLoginId;
+	@Column(nullable = false)
 	private String userPassWord;
-	@Column(unique = true)
+	@Column(unique = true,nullable = false)
 	private String userEmail;
+
+	@Column(nullable = false)
 	private String userAddress;
+	@Column(nullable = false)
 	private String userPhone;
+	@Column(nullable = false)
 	private String userBirth;
+	@Column(nullable = false)
 	private String userGender;
+	@Column(nullable = false)
 	private String userNickName;
+	@Column(unique = true ,nullable = false)
 	private String userCivilSerial;
 
 	@CreationTimestamp
+	@Column(nullable = false)
     private Timestamp userCreatedAt;
+	@JoinColumn(nullable = false)
 	private int accessLevel;
-	private boolean userLockYn;
-	private int loginAttemptCont;
+	private boolean userLockYn = false;
+	private int loginAttemptCount = 0;
 
-    @Builder
-    public User(int id, String username, String password, String email, Timestamp createdAt) {
-        this.user_id = id;
-        this.username = username;
-        this.userPassWord = password;
-        this.userEmail = email;
-        this.userCreatedAt = createdAt;
-    }
+//    @Builder
+//    public User(int id, String username, String password, String email, Timestamp createdAt) {
+//        this.userId = id;
+//        this.username = username;
+//        this.userPassWord = password;
+//        this.userEmail = email;
+//        this.userCreatedAt = createdAt;
+//    }
 }
