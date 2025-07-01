0211ea36b861cfcb752e7e807e18b7c4ab18995a
jibaek <tngus000777@gmail.com>
2025-07-01 17:04:24 +0900

    마이페이지,회원 정보 수정,이력서 화면 1차 생성 완료

--- src/main/java/com/jobgoalin/workinfo/mypage/MyPageController.java
+++ src/main/java/com/jobgoalin/workinfo/mypage/MyPageController.java
@@ -1,11 +1,29 @@
 package com.jobgoalin.workinfo.mypage;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@RequiredArgsConstructor
+@Controller
 public class MyPageController {
 
-    @GetMapping("/")
+
+    @GetMapping("/my_page")
+    public String myPage() {
+        return "my_page";
+    }
+
+    // http://localhost:8080/filelist
+
+    @GetMapping("/edit_profile")
     public String updateUserInfo() {
-        return "filelist";
+        return "edit_profile";
+    }
+
+    @GetMapping("/resume_list")
+    public String resume() {
+        return "resume_list";
     }
 }
