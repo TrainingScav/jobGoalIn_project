9349436418ae70ef5ef46e47c6913a48686fb512
TrainingScav <dnslwk123@naver.com>
2025-07-01 17:19:13 +0900

    기업목록 추가 작업 예정, 안쓰는 css파일 삭제, 로고 추가

--- src/main/java/com/jobgoalin/workinfo/company/CompanyController.java
+++ src/main/java/com/jobgoalin/workinfo/company/CompanyController.java
@@ -1,8 +1,15 @@
 package com.jobgoalin.workinfo.company;
 
+import com.jobgoalin.workinfo.info.CompanyInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class CompanyController {
@@ -10,11 +17,28 @@ public class CompanyController {
     @Autowired
     private CompanyService companyService;
 
+    private static final Logger log = LoggerFactory.getLogger(CompanyController.class);
+
     @GetMapping("/company/list")
-    public String companyInfoList() {
+    public String companyInfoList(Model model) {
 
+        List<CompanyInfo> companyInfoList = companyService.findAllCompanyInfo();
 
+        log.info("companyInfoList : {}", companyInfoList);
 
+        model.addAttribute("companyInfoList", companyInfoList);
         return "company/company_list";
+    }
+
+    @GetMapping("/company/{id}")
+    public String companyInfoDetail(@PathVariable(name = "id") Long id, Model model) {
+
+        try {
+            CompanyInfo companyInfoDetail = companyService.findCompanyInfoById(id);
+            model.addAttribute("companyInfo", companyInfoDetail);
+            return "company/company_detail";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/jobgoalin/workinfo/company/CompanyJpaRepository.java
+++ src/main/java/com/jobgoalin/workinfo/company/CompanyJpaRepository.java
@@ -2,6 +2,11 @@ package com.jobgoalin.workinfo.company;
 
 import com.jobgoalin.workinfo.info.CompanyInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CompanyJpaRepository extends JpaRepository<CompanyInfo, Long> {
+
 }
