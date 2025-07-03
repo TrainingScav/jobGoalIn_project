dda81d941c8ccf54206c5da6c45b1cdc0ed01617
jibaek <tngus000777@gmail.com>
2025-07-03 12:16:18 +0900

    회원 정보 수정 기능 구현 완료

--- src/main/java/com/jobgoalin/workinfo/user/UserController.java
+++ src/main/java/com/jobgoalin/workinfo/user/UserController.java
@@ -2,14 +2,19 @@ package com.jobgoalin.workinfo.user;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     // 회원가입 화면 요청
@@ -53,4 +58,37 @@ public class UserController {
         session.invalidate();
         return "redirect:/login";
     }
+
+    /**
+     * 회원 정보 수정 화면 요청
+     */
+    @GetMapping("update-form")
+    public String updateForm(Model model,HttpSession session) {
+
+
+        User checkSesssionUser = (User)session.getAttribute("sessionUser");
+        if (checkSesssionUser != null) {
+            log.info("sessionUser 값 확인 : {}", checkSesssionUser.toString());
+        }
+
+        User user = userService.findById(1L);
+
+        model.addAttribute("userInfo", user);
+
+        return "update-form";
+    }
+
+    /**
+     * 회원 수정 기능 요청
+     */
+
+    @PostMapping("/update/{id}")
+    public String update(@PathVariable(name = "id") Long id, UserRequest.UpdateDTO reqDTO, HttpSession session) {
+        reqDTO.validate();
+        User user = (User)session.getAttribute("sessionUser");
+        User updateUser = userService.updateById(id,reqDTO);
+        session.setAttribute("sessionUser",updateUser);
+        return "redirect:/update-form";
+    }
+
 }
--- src/main/java/com/jobgoalin/workinfo/user/UserService.java
+++ src/main/java/com/jobgoalin/workinfo/user/UserService.java
@@ -1,7 +1,12 @@
 package com.jobgoalin.workinfo.user;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -9,7 +14,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
 
     // 회원가입 - 유니크 컬럼만 검증함
@@ -33,7 +38,7 @@ public class UserService {
                 .userPhone(dto.getUserPhone())
                 .userBirth(dto.getUserBirth())
                 .userGender(dto.getUserGender())
-                .userNickName(dto.getUserNickName())
+                .userNickName(dto.getUserNickname())
                 .userCivilSerial(dto.getUserCivilSerial())
                 .userCreatedAt(LocalDateTime.now())
                 .accessLevel(1L)
@@ -54,8 +59,33 @@ public class UserService {
                 return user;
             }
         }
-
         return null; // 아이디나 비밀번호 없으면 null 반환
+    }
+
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> {
+            log.warn("사용자 조회 실패 - ID: {}", id);
+            return new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+        });
+    }
+
+
+    /**
+     *  회원정보 수정 처리 (더티 체킹)
+     */
+
+    @Transactional
+    public User updateById(Long userId,UserRequest.UpdateDTO updateDTO) {
+
+        User user = findById(userId);
+
+        // 더티 체킹
+        user.setUserNickName(updateDTO.getUserNickname());
+        user.setUserPhone(updateDTO.getUserPhoneNumber());
+        user.setUserPassWord(updateDTO.getUserPassword());
+        user.setUserAddress(updateDTO.getUserAddress());
+        return user;
     }
 
 }
