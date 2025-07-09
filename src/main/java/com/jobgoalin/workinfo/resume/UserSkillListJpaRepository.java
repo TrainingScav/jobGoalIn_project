ad6ad6fe4352f9a0adfed0809ddcb12931e2a01b
jibaek <tngus000777@gmail.com>
2025-07-09 18:12:21 +0900

    마이페이지 화면 작업완료

--- src/main/java/com/jobgoalin/workinfo/resume/UserSkillListJpaRepository.java
+++ src/main/java/com/jobgoalin/workinfo/resume/UserSkillListJpaRepository.java
@@ -2,13 +2,7 @@ package com.jobgoalin.workinfo.resume;
 
 import com.jobgoalin.workinfo.user.UserSkillList;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface UserSkillListJpaRepository extends JpaRepository<UserSkillList, Long> {
-    List<UserSkillList> findByUser_UserId(Long userId);
-    @Query("select usl from UserSkillList usl join fetch usl.skillList where usl.user.userId = :userId")
-    List<UserSkillList> findByUserWithSkillFetch(@Param("userId") Long userId);
+
 }
